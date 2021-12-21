0f76645e7a2eca89eb6d06a9de74bf598c0840dc
Theodor Khademi <theo.khademi@gmail.com>
2021-12-21 13:08:25 +0100

    more cleanup, added how to play page, added todos

--- src/OddsOn.js
+++ src/OddsOn.js
@@ -1,11 +1,14 @@
-// import logo from './logo.svg';
 import './css/OddsOn.css';
+import { HowToPlay } from './pages/HowToPlay.js';
+import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 
+// TODO: add a route to the how to play page 
 function OddsOn() {
   return (
-    <div >
-      yoyoyo 
-    </div>
+      <div >
+        yoyoyo 
+      </div>
+    
   );
 }
 
---
+++ src/pages/HowToPlay.js
@@ -0,0 +1,11 @@
+export const HowToPlay = () => { 
+
+    // TODO: add a description of how to play
+    return (
+        <div> 
+            this is how you play 
+        </div>
+    ) 
+}
+
+export default HowToPlay
